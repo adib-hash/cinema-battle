f21b7b0abc080429efa0d764de435cab544331ec
adib-hash <adib@ihsan.build>
2026-02-20 22:40:23 -0800

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CinemaBattle from './CinemaBattle.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CinemaBattle />
+  </React.StrictMode>
+)
